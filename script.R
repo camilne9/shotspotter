7fb2b8e3351a3810f9c3be3d8e96a253eccd3bfc
Christopher Milne <christophermilne@Christophers-MacBook-Pro.local>
2019-04-18 14:39:36 -0400

    Map of Worcester created (without other data yet).

--- script.R
+++ script.R
@@ -2,23 +2,49 @@ library(tidyverse)
 library(readr)
 library(janitor)
 library(tigris)
+library(sf)
+library(ggthemes)
+library(fs)
+library(gganimate)
 
-brockton <- read_csv("Brockton_ShotSpotterCSV.csv",
-                     col_types = cols(
-                       `Incident Id` = col_double(),
-                       `CAD Id` = col_character(),
-                       Time = col_datetime(format = ""),
-                       Type = col_character(),
-                       Address = col_character(),
-                       Latitude = col_double(),
-                       Longitude = col_double(),
-                       `# of Rounds` = col_double(),
-                       District = col_logical(),
-                       Beat = col_logical()
-                     )) %>% 
+#brockton <- read_csv("Brockton_ShotSpotterCSV.csv",
+ #                    col_types = cols(
+  #                     `Incident Id` = col_double(),
+   #                    `CAD Id` = col_character(),
+    #                   Time = col_datetime(format = ""),
+     #                  Type = col_character(),
+      #                 Address = col_character(),
+       #                Latitude = col_double(),
+        #               Longitude = col_double(),
+         #              `# of Rounds` = col_double(),
+          #             District = col_logical(),
+           #            Beat = col_logical()
+            #         )) %>% 
+#  clean_names()
+
+worcester <- read_csv("worcester_ma.csv",
+                      col_types = cols(
+                        Date = col_character(),
+                        `Time Range` = col_character(),
+                        Latitude = col_number(),
+                        Longitude = col_number(),
+                        `Incident Type` = col_character(),
+                        `Round Count` = col_character()
+                      )) %>% 
   clean_names()
 
-#x <- call_geolocator("Brockton", "MA")
-shapes <- urban_areas(class = "sf") %>% 
-  mutate(brockton = str_detect(NAME10, ""))
+raw_shapes <- urban_areas(class = "sf") 
+worcester_shapes <- raw_shapes %>% 
+  mutate(worcester = str_detect(NAME10, "Worcester")) %>% 
+  filter(worcester)
+
+gun_locations <- st_as_sf(worcester,
+                          coords = c("longitude", "latitude"),
+                          crs = 4326)
+ggplot(data = worcester_shapes)  +
+  geom_sf()+
+  #geom_sf(data = gun_locations, aes(color = incident_type, fill = incident_type))+
+  theme_map()+
+  theme_solarized()
+  
 
