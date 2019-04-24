058eb507395731bb091dde305ba38f4195b3a0b9
Christopher Milne <christophermilne@dhcp-10-250-246-25.harvard.edu>
2019-04-24 00:02:59 -0400

    Completed shiny app code (without comments).

--- Shotspotter/app.R
+++ Shotspotter/app.R
@@ -13,11 +13,21 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Machine Gun Shots in Springfield MA"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
      sidebarPanel(
+       h4("Description:"),
+p("This animation uses Shotspotter data 
+from Springfield MA to examine the 
+                    location of machine gun shots in Springfield between 2008 and 2018. 
+                    Thank you to Justice Tech Lab for making the data used in creating this plot available."),
+uiOutput("shotspotter"),
+h4("Github:"),
+uiOutput("link"),
+h3("Creator:"),
+h4("Christopher Milne")
      ),
       
       # Show a plot of the generated distribution
@@ -29,6 +39,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  source<- a("here.", href="http://justicetechlab.org/shotspotter-data/")
+  
+  url <- a("here.", href="https://github.com/camilne9/shotspotter")
+  
+  output$shotspotter <- renderUI({
+    tagList("You can find the complete Shotspotter data", source)})
+  
+  output$link <- renderUI({
+    tagList("You can find the github repository creating this animation", url)})
+  
    output$test <- renderImage(expr = list(src = "springfield.gif"), 
                               deleteFile = FALSE)
 }
